3c6db5d8525f8265db55fe671f5792a5c5a9d6e0
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-09 15:12:23 +0900

    createdメソッドにログアウトメソッドを追記

--- src/views/LogoutAdmin.vue
+++ src/views/LogoutAdmin.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="logoutAdmin"></div>
+  <div>dubby</div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import axios from "axios";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LogoutAdmin extends Vue {
+  /**
+   * ログアウトをする.
+   */
+  async created(): Promise<void> {
+    const response = await axios.get(
+      "http://34.214.207.222:8080/ex-emp-api/logout"
+    );
+    if (response.data.status === "success") {
+      this["$router"].push("/loginAdmin");
+    }
+  }
+}
 </script>
 
 <style></style>
